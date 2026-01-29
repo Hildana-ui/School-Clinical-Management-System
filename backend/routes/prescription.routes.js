ea0368b51aec19b3faf4c2605a975d4c3bb0668a
Hildana-ui <hildana.amare@bitscollege.edu.et>
2026-01-29 20:41:35 +0300

    Finished Prescriptions, Student search profile and student search page

--- backend/routes/prescription.routes.js
+++ backend/routes/prescription.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const prescriptionController = require('../controllers/prescription.controller');
 
 router.post('/', prescriptionController.addPrescription);
-router.put(':id', prescriptionController.updatePrescription);
+router.put('/:id', prescriptionController.updatePrescription);
 
 module.exports = router;
